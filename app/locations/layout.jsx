aaf278d4e3bee5a4049ab5a148f95d3bea9cfa46
pawan8120 <lit2021008@iiitl.ac.in>
2024-01-21 01:36:34 +0530

    adding dynamic routing of locations

---
+++ app/locations/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Location = ({ children }) => {
+  return (
+    <div>
+      this is layout of location
+      {children}
+    </div>
+  )
+}
+
+export default Location
