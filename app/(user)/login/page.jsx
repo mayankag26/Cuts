a3409e8cd256188acb36eee343d4373471327829
Mayank Agarwal <agmayank26@gmail.com>
2024-01-29 13:23:04 +0530

    Login, signup for user

---
+++ app/(user)/login/page.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "./LoginForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Login() {
+  const session = await getServerSession(authOptions);
+
+  if (session) redirect("/");
+
+  return <LoginForm />;
+}
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import {Providers} from "./providers";
 import Nav from './components/navbar';
 import Footer from './components/footer';
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,11 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <Nav/>
-          {children}
-          <Footer/>
-        </Providers>
+          <Providers>
+            <Nav/>
+            {children}
+            <Footer/>
+          </Providers>
       </body>
     </html>
   )
